e484188303eab876bf25c52c9ac37581653600b0
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2024-06-15 21:50:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,8 +18,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/ImmortalWrt/R619AC/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/WR1200JS/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/R619AC/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/WR1200JS/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/PandoraBox/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #sed -i '8i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
