d173c1d8759371dd2665460cde55cd6609bb2bb6
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2024-06-22 17:42:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/R619AC/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/R619AC/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/WR1200JS/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/PandoraBox/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
