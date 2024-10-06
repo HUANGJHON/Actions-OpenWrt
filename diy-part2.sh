048e47bfa618fd6d3ac4e23a2897404ab1781673
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2024-10-07 00:16:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
@@ -19,7 +19,7 @@ sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 
 # Modify hostname
 #sed -i 's/ImmortalWrt/R619AC/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/WR1200JS/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/WR1200JS/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/PandoraBox/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #sed -i '8i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
