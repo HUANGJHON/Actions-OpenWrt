87bcf34772fb463fda181da9ad9a2ce6ab5e43ba
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2024-11-01 01:28:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
+#git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
 #git clone https://github.com/HUANGJHON/turboacc.git package/turboacc
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone -b package --single-branch https://github.com/chenmozhijin/turboacc.git package/shortcut-fe
 
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
