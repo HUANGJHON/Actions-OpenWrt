9c582434f31afd8ca41b0f636f4bb747c6cd84a8
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2024-11-08 01:03:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone -b package --single-branch https://github.com/chenmozhijin/turboacc.git package/shortcut-fe
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
