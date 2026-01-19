d2aa9cc0811958a9ddfbfb6b5e413203271469f8
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2026-01-19 23:15:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
 #git clone https://github.com/kenzok8/openwrt-packages.git package/small
 #git clone https://github.com/xuanranran/openwrt-package.git package/xuanranran
-git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/HUANGJHON/turboacc.git package/turboacc
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone -b package --single-branch https://github.com/chenmozhijin/turboacc.git package/shortcut-fe
